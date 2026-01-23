92b47f637ed67be95b2b28d65634127d20a087ee
BelAlmendras <belen.almendros19@gmail.com>
2026-01-23 01:27:52 -0300

    test: add test products and login

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+    test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.js",
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
