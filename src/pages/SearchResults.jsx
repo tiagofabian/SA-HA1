e43b33890a5f3535eedb478552946edef4a238f1
Tiago Alcázar <tiagofabian195@outlook.com>
2026-01-26 11:02:17 -0300

    fix duplicity product

--- src/pages/SearchResults.jsx
+++ src/pages/SearchResults.jsx
@@ -55,7 +55,7 @@ const SearchResults = ({ itemsPerPage = 8 }) => {
       </h1>
 
       {!imagesReady && (
-        <p className="text-center text-gray-400 mb-6 min-h-screen">Cargando imágenes…</p>
+        <p className="text-center text-gray-400 mb-6 ">Cargando imágenes…</p>
       )}
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 mb-8">
